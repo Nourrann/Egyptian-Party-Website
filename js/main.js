caefbfa4fcb2d79d62aed6f15bfa7a1ca34b8b72
Nouran Ahmed <noranahmed5617@gmail.com>
2024-03-13 23:04:03 +0200

    update

--- js/main.js
+++ js/main.js
@@ -56,3 +56,22 @@ $(".textareaCount").keyup(function () {
   $("#counter").text(100 - textLength <= 0 ? "No" : 100 - textLength);
 });
 //^ End Contact Action
+
+document.querySelectorAll("nav a").forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const targetId = this.getAttribute("href").substring(1);
+    const targetElement = document.getElementById(targetId);
+
+    if (targetElement) {
+      const yOffset = -180; // Adjust this value based on your padding
+      const y =
+        targetElement.getBoundingClientRect().top +
+        window.pageYOffset +
+        yOffset;
+
+      window.scrollTo({ top: y, behavior: "smooth" });
+    }
+  });
+});
